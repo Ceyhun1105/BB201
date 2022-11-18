f5ddafff1f989d7035a0028807de240905010b38
Ceyhun <116638807+Ceyhun1105@users.noreply.github.com>
2022-11-18 05:15:47 +0400

    18.11.2022

---
+++ November 18/18.11.2022/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _11._18._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a=string.Empty;
+            a=EnterValue(a);
+            CheckValue(a,ref a);
+            string b=string.Empty;
+            b=EnterValue(b);
+            CheckValue(b,ref b);
+            int newA = Convert.ToInt32(a);
+            int newB = Convert.ToInt32(b);
+            Console.WriteLine();
+            Console.WriteLine($"{a}+{b}={Sum(newA, newB)}");
+        }
+
+
+        static string EnterValue(string str)
+        {
+            Console.Write("Enter the Number : ");
+            str = Console.ReadLine();
+            return str;
+        }
+        static bool CanChangeInt(string str)
+        {
+            bool CanChangeInt = Int32.TryParse(str, out int value);
+            return CanChangeInt;
+        }
+        static void CheckValue(string str, ref string str1)
+        {
+            bool check = true;
+            do
+            {
+                if (CanChangeInt(str) == true)
+                {
+                    check = false;
+                }
+                else
+                {
+                    Console.Write("   Your String can not change to int !!!  ");
+                    str = EnterValue(str);
+                    str1 = str;
+                }
+            } while (check == true);
+        }
+        static int Sum(int num1,int num2)
+            {
+                return num1 + num2;
+            }
+    }
+}
---
+++ November 18/18.11.2022_2/Program.cs
@@ -0,0 +1,84 @@
+﻿
+namespace _18._11._2022_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[1];
+            numbers[0] = 1;
+            bool ContinueStep = true;
+            bool Stop = false;
+            do
+            {
+                if (ContinueStep == false)
+                {
+                    Console.Write("\n Do you want to comtinue? Answer : ");
+                    string answer = Console.ReadLine();
+                    if (answer == "yes" | answer == "Yes")
+                    {
+                        string value = string.Empty;
+                        string newvalue = EnterNum(value);
+                        ResizeArray(ref numbers, newvalue);
+
+
+                    }
+                    else
+                    {
+                        Stop = true;
+                    }
+                }
+                else
+                {
+                    string value = string.Empty;
+                    string newvalue = EnterNum(value);
+                    ResizeArray(ref numbers, newvalue);
+
+                }
+
+
+
+
+                ContinueStep = false;
+
+                Console.WriteLine();
+
+
+
+
+            } while (Stop == false);
+
+        }
+        static string EnterNum(string str1)
+        {
+            Console.Write(" Enter the number : ");
+            str1 = Console.ReadLine();
+            return str1;
+        }
+        static bool IsNumber(string str1)
+        {
+            bool IsTrue = Int32.TryParse(str1, out int n);
+            return IsTrue;
+        }
+        static void ResizeArray(ref int[] arr, string str1)
+        {
+
+            if (IsNumber(str1) == true)
+            {
+                Array.Resize(ref arr, arr.Length + 1);
+                arr[arr.Length - 1] = Convert.ToInt32(str1);
+                Console.WriteLine($" Change your Array`s Length : {arr.Length}\n");
+                Console.WriteLine(" Array is changed... \n");
+                foreach (int num in arr)
+                {
+                    Console.Write(" " + num);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n Array doesn`t change");
+            }
+        }
+
+    }
+}
