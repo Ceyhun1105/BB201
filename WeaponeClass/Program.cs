96d1dcd4b99afb72960b307f2dfda31f4834a452
Ceyhun <116638807+Ceyhun1105@users.noreply.github.com>
2022-11-24 02:44:53 +0400

    24.11.2022

---
+++ WeaponeClass/Program.cs
@@ -0,0 +1,82 @@
+﻿using WeaponeClass;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int BulletCapacity;
+        int BulletCount;
+        double Dischargertime;
+        bool IsAuto;
+        bool check;
+        do
+        {
+            Console.Write(" Enter BulletCapacity : ");
+            check = int.TryParse(Console.ReadLine(), out BulletCapacity);
+        } while (!check);
+
+        do
+        {
+            Console.Write(" Enter BulletCount : ");
+            check = int.TryParse(Console.ReadLine(), out BulletCount);
+        } while (!check);
+
+        do
+        {
+            Console.Write(" Enter Discharge time : ");
+            check = double.TryParse(Console.ReadLine(), out Dischargertime);
+        } while (!check);
+
+        do
+        {
+            Console.Write(" Enter the Mod ( default Auto ) : ");
+            check = bool.TryParse(Console.ReadLine(), out IsAuto);
+        } while (!check);
+
+
+        Weapone weapon1 = new Weapone(BulletCapacity, BulletCount, Dischargertime, IsAuto);
+        
+        string choice;
+        do
+        {
+            Console.WriteLine("\n 0 - See Weapone Information\n" +
+                " 1 - Bring Shoot Method\n" +
+                " 2 - Bring Fire Method\n" +
+                " 3 - return you need number of bullet for full capacity\n" +
+                " 4 - Reload\n" +
+                " 5 - Change Fire Mode\n" +
+                " 6 - Close App");
+            Console.Write("\n Enter your Choice : ");
+            choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "0":
+                    weapon1.WeaponeInfo();
+                    break;
+                case "1":
+                    weapon1.Shoot();
+                    break;
+                case "2":
+                    weapon1.Fire();
+                    break;
+                case "3":
+                    Console.WriteLine($" You need {weapon1.GetRemainBulletCount()} bullet for full capacity");
+                    break;
+                case "4":
+                    weapon1.Reload();
+                    break;
+                case "5":
+                    weapon1.ChangeFireMode();
+                    break;
+                case "6":
+                    break;
+                default:
+                    Console.WriteLine(" Our Service has not your choice ");
+                    break;
+            }
+        } while (choice!="6");
+
+
+
+    }
+}
---
+++ WeaponeClass/Weapone.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponeClass
+{
+    internal class Weapone
+    {
+        public Weapone(int bulletcapacity, int bulletcount, double dischargetime, bool auto)
+        {
+            this.BulletCapacity = bulletcapacity;
+            this.BulletCount = bulletcount;
+            this.Dischargetime = dischargetime;
+            this.IsAuto = auto;
+        }
+
+
+        private int _bulletcapacity;
+        private int _bulletcount;
+        private double _dischargetime;
+
+        public bool IsAuto;
+        public int BulletCapacity
+        {
+            get { return _bulletcapacity; }
+            set
+            {
+                if (value > 3 && value < 180)
+                {
+                    _bulletcapacity = value;
+                }
+            }
+        }
+        public int BulletCount
+        {
+            get { return _bulletcount; }
+            set
+            {
+                if (value < BulletCapacity && value > 0)
+                {
+                    _bulletcount = value;
+                }
+            }
+        }
+
+        public double Dischargetime
+        {
+            get { return _dischargetime; }
+            set
+            {
+                if (value > 2 && value < 10)
+                {
+                    _dischargetime = value;
+                }
+            }
+        }
+
+        public void WeaponeInfo()
+        {
+            Console.WriteLine($"\n capacity : {BulletCapacity}\n bulletcount : {BulletCount}\n discharger time : {Dischargetime}\n AutoMode : {IsAuto}");
+        }
+
+        public void Shoot()
+        {
+            if (BulletCount > 0)
+            {
+                Console.WriteLine("\n Shooted 1 bullet ");
+                BulletCount--;
+                Console.WriteLine($" Weapone has {BulletCount} bullet");
+            }
+            else
+            {
+                Console.WriteLine("\n Weapone do not have any bullet\n You can use Reload ");
+            }
+        }
+
+        public void Fire()
+        {
+            if (IsAuto == true)
+            {
+                if (BulletCount != 0)
+                {
+                    double time = Dischargetime / BulletCapacity * BulletCount;
+
+                    _bulletcount = 0;
+                    Console.WriteLine(" Fired all bullet ..");
+
+
+                    Console.WriteLine($" Bullets shooted for {time} second ");
+                }
+                else
+                {
+                    Console.WriteLine("\n Weapone do not have any bullet\n You can use Reload ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n Can not All bullet in Single Mode Fire ... ");
+                Shoot();
+            }
+        }
+
+        public int GetRemainBulletCount()
+        {
+            return BulletCapacity - BulletCount;
+        }
+
+        public void Reload()
+        {
+            if (BulletCount != BulletCapacity)
+            {
+                //BulletCount=BulletCapacity;
+                _bulletcount += GetRemainBulletCount();
+                Console.WriteLine($" Reload weapone . You have {BulletCapacity} bullet");
+            }
+            else
+            {
+                Console.WriteLine("\n Capacity is Full .. You can Fire or Shoot...");
+            }
+        }
+
+        public void ChangeFireMode()
+        {
+            if (IsAuto == true)
+            {
+                IsAuto = false;
+                Console.WriteLine(" Fired Mode Changed Single Mode..");
+            }
+            else
+            {
+                IsAuto = true;
+                Console.WriteLine(" Fired Mode Changed Auto Mode..");
+            }
+        }
+
+
+
+    }
+}
