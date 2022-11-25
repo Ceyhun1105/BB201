82b24f2c7d1f5ecb00e84903796a2733a96f3ec5
Ceyhun <116638807+Ceyhun1105@users.noreply.github.com>
2022-11-25 04:16:10 +0400

    25.11.2022   LibraryClass

---
+++ 25 November/ClassLibrary/User.cs
@@ -0,0 +1,111 @@
+﻿
+namespace ClassLibrary
+{
+    public class User
+    {
+        public User(string userName)
+        {
+            Username = userName;
+        }
+
+        
+        
+        private string _username;
+        private string _password;
+
+        public string Username { get => _username;
+            set
+            {   if (!String.IsNullOrWhiteSpace(value) && value.Length >= 6 && value.Length <= 25)
+                {
+                    _username = value;
+                }
+            }
+        }
+        public string Password {
+            get => _password;
+            set
+            {
+                if(!String.IsNullOrWhiteSpace(value) && value.Length>=8 && value.Length <= 25)
+                {
+                    _password = value;
+                }
+            }
+        }
+
+        public static bool HasDigit(string str)
+        {
+            bool have=false;
+            for(int i=0;i<str.Length;i++) 
+            {
+                have = Char.IsDigit(str[i]);
+                if (have) break;
+            }
+            return have;
+        }
+        public static bool HasUpper(string str)
+        {
+            bool have = false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                have = Char.IsUpper(str[i]);
+                if (have) break;
+            }
+            return have;
+        }
+        public static bool HasLower(string str)
+        {
+            bool have = false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                have = Char.IsLower(str[i]);
+                if (have) break;
+            }
+            return have;
+        }
+
+
+
+        // Addition
+
+
+        private string _bio;
+
+        public string Bio { get; set; }
+        public void ChangePhoto()
+        {
+            Console.ForegroundColor= ConsoleColor.Blue;
+            Console.WriteLine(" Your Profile Photo are changed ");
+            Console.ResetColor();
+        }
+        public void ChangeBio()
+        {
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.Write(" Enter new Bio : ");
+            string bio = Console.ReadLine();
+            if (!String.IsNullOrEmpty(bio))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine(" Bio are changed ... ");
+                Bio = bio;
+            }
+
+            Console.ResetColor();
+        }
+        public void ShowBio() 
+        {
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            if (String.IsNullOrEmpty(Bio))
+            {
+                Console.WriteLine(" You have not Bio ... You can Add ...");
+            }
+            else
+            {
+                Console.WriteLine($" Your Bio : {Bio}");
+            }
+            Console.ResetColor();
+        }
+
+
+        
+    }
+}
---
+++ 25 November/LogInClass/Program.cs
@@ -0,0 +1,79 @@
+﻿using ClassLibrary;
+namespace LogInClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = String.Empty;
+            string password = String.Empty;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(" Username : ");
+                Console.ResetColor();
+                username = Console.ReadLine();
+                if (username.Length < 6 | username.Length>25)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" Username must be minimum 6 and maximum 25 letters !");
+                    Console.ResetColor();
+                }
+
+            } while (String.IsNullOrWhiteSpace(username) | username.Length<6 | username.Length > 25);
+
+
+            bool checkpassword = false;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(" Password : ");
+                Console.ResetColor();
+                password = Console.ReadLine();
+                if(!String.IsNullOrWhiteSpace(username) && password.Length >= 8 && password.Length <= 25 &&
+                   User.HasDigit(password) && User.HasUpper(password) && User.HasLower(password))
+                {
+                    checkpassword = true;
+                }
+                else
+                {
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    if (password.Length < 8 | password.Length > 25)
+                    {
+                        Console.WriteLine(" Password must be minimum 8 and maximum 25 letters !");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" Your password must contain :\n" +
+                            " at least 1 lowercase letter\n at least 1 uppercase letter\n at least 1 digit  ");
+                    }
+                    Console.ResetColor();
+                }
+
+            } while (!checkpassword);
+
+
+            User user1 = new User(username);
+            user1.Password = password;
+
+            //Addition 
+
+            Console.WriteLine($" Welcome {user1.Username} . We hope you are Excellent...\n Loading ...");
+
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+
+            System.Threading.Thread.Sleep(1000);
+            user1.ChangePhoto();
+
+            user1.ShowBio();
+
+            user1.ChangeBio();
+
+            user1.ShowBio();
+
+            
+        }
+
+    }
+}
