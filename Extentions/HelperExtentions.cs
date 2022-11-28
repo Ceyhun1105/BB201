0b53734e4e8fccd6d21cce9d460046b3f27e0bad
Ceyhun <116638807+Ceyhun1105@users.noreply.github.com>
2022-11-29 02:23:25 +0400

    28.11.2022 Extentions

---
+++ Extentions/HelperExtentions.cs
@@ -0,0 +1,61 @@
+﻿
+namespace Extentions
+{
+    public static class HelperExtentions
+    {
+
+
+        public static int MatchCount(this string sentence, string reply)
+        {
+
+            int count = 0;
+            for (int i = 0; i < sentence.Length; i++)  // ananas1 an
+            {
+                
+                bool possible = true;
+                if (i == sentence.Length - reply.Length+1) break;
+                if (sentence[i] != reply[0]) continue;
+                
+                
+                    for (int j = i+1, t = 1; j < i + reply.Length; j++, t++)
+                    {
+                        if (sentence[j] != reply[t]) possible = false;
+                    }
+                    if (possible) count++;
+                
+            }
+            return count;
+        }
+
+        public static bool HasAnyDigit(this string sentence) 
+        {
+            foreach(char item in sentence)
+            {
+                if(char.IsDigit(item)) return true;
+            }
+            return false;
+        }
+        
+        public static bool Polindrom(this int number) 
+        {
+           
+            string num = number.ToString();  // 4444
+            int center = num.Length / 2;   // 1 
+            string reverse=String.Empty;
+            for(int i=num.Length-1;i>center-1 ;i--)
+            {
+                reverse += num[i];
+            }
+            if (num.Length % 2 == 0)
+            {
+                if (num.Substring(0, center) == reverse) return true;
+                return false;
+            }
+            else
+            {
+                if (num.Substring(0, center + 1 ) == reverse) return true;
+                return false;
+            }
+        }
+    }
+}
---
+++ Extentions/Program.cs
@@ -0,0 +1,20 @@
+﻿using Extentions;
+namespace Extentions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "ananas1";
+            Console.WriteLine(" how many an in ananas1? : "+name.MatchCount("an"));
+            Console.WriteLine(" ananas1 has any digit? : "+name.HasAnyDigit());
+            name = "ananas";
+            Console.WriteLine(" how many a in ananas1? : " + name.MatchCount("a"));
+            Console.WriteLine(" ananas has any digit? : " + name.HasAnyDigit());
+            int number = 3553;
+            Console.WriteLine(" First Step 3553 Polindromdur ? : "+number.Polindrom());
+            number = 3555;
+            Console.WriteLine(" Second Step 3555 Polindromdur ? : " + number.Polindrom());
+        }
+    }
+}
