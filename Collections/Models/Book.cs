33caf3b01609b8f62af4de417319ea30ae0ff2b0
Ceyhun <116638807+Ceyhun1105@users.noreply.github.com>
2022-12-03 17:32:10 +0400

    02.12.2022 Collections

---
+++ Collections/Models/Book.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System.Data;
+
+namespace Collections.Models
+{
+    internal class Book
+    {
+        static int _counter;
+
+        public string Name { get; set; }
+        
+        public string AuthorName { get; set; }
+
+        public int PageCount { get; set; }
+
+        public string Code { get; set; } 
+
+        public Book(string name, string authorname, int count)
+        {
+            _counter++;
+            Name = name;
+            AuthorName=authorname;
+            PageCount=count;
+            Code= Name.Trim().Substring(0, 2).ToUpper() + _counter.ToString();
+        }
+
+        public override string ToString()
+        {
+            return $" Name : {Name}\n Author Name : {AuthorName}\n Code : {Code}\n Number of Page : {PageCount}";
+        }
+
+    }
+}
