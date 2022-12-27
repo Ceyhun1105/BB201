7ca68295f432bcd1a5b2a1b6dc2ba1d3aa2cf0d6
Ceyhun <116638807+Ceyhun1105@users.noreply.github.com>
2022-12-28 01:43:13 +0400

    25.12.2022

---
+++ MSSQL/ReplyLesson/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmptyProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["name"] = "Ceyhun";
+            int num = 19;
+            return View(num);
+        }
+        public IActionResult Contact()
+        {
+            ViewBag.age = 19;
+            string name = "Ceyhun";
+            return View((object)name);
+        }
+    }
+}
